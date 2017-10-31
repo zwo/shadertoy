34823f4e42a48dffe1b7a3c6fb737e4031013af6
zhouweiou <zhouweiou@mac.local>
2017-10-31 09:51:37 +0800

    circle only

---
+++ wave.glsl
@@ -0,0 +1,20 @@
+vec3 HexTo01Color(float redAmount,float greenAmount,float yellowAmount){
+    vec3 color = vec3(redAmount/255.,greenAmount/255.,yellowAmount/255.);
+    return color;
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+	vec2 uv = (2.*fragCoord-iResolution.xy)/min(iResolution.x,iResolution.y);//将屏幕空间映射至[-1.xx,1.xx]x[-1,1]（width大于height）
+	vec3 color_1 = HexTo01Color(22.,147.,165.);
+    vec3 color_2 = HexTo01Color(173.,216.,199.);
+    vec3 color_3 = HexTo01Color(251.,184.,41.);
+	vec3 BGcolor = vec3(1.);
+    vec3 color=BGcolor;
+
+    vec2 circlePosition = vec2(0.2,0.2);
+    if(length(uv-circlePosition)<0.1) color = color_3;
+
+       
+     fragColor = vec4(color,1.);    
+}
